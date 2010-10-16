e922230215fb77f98d1f61949e053d2c86c6bd04
Matthew McCullough <matthewm@ambientideas.com>
2010-10-15 20:23:36 -0500

    Refactored conversion script to handle new subdirectory locations

--- markdown-all-docs.sh
+++ markdown-all-docs.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-rm *.md.html
-rm *.pdf
+pushd workbook
 
+echo Cleaning up old PDFs
+rm pdfs/*.pdf
+echo Cleaning up old HTMLs
+rm htmls/*.md.html
+
+pushd markdown
+echo Markdown to PDF conversion...
 find . -name "*.md" -exec markdown2pdf {} \;
+echo Markdown to HTML conversion...
 find . -name "*.md" -exec markdownhere {} \;
+
+echo Relocating PDFs to output directory...
+mv *.pdf ../pdfs/
+echo Relocating HTMLs to output directory...
+mv *.html ../htmls/
+popd
+
+popd
